f4be7c6e03582bbb46c64e6e80c0df0ff81621d2
rayhomie <1572801584@qq.com>
2021-07-02 11:28:08 +0800

    20.双指针解法

--- 20.合并两个有序数组.js
+++ 20.合并两个有序数组.js
@@ -21,24 +21,26 @@ var merge = function (nums1, m, nums2, n) {
 };
 
 
-//方法二：双指针
+//方法二：双指针https://leetcode-cn.com/problems/merge-sorted-array/solution/he-bing-liang-ge-you-xu-shu-zu-by-leetco-rrb0/
 var merge = function (nums1, m, nums2, n) {
   let p1 = 0, p2 = 0;
   const sorted = new Array(m + n).fill(0);
   var cur;
-  while (p1 < m || p2 < n) {
-    if (p1 === m) {
+  while (p1 < m || p2 < n) {//数组1和数组2只要没有遍历完成，就会执行循环
+    if (p1 === m) {//当数组1被遍历完，下一次遍历数组2，并取出当前值
       cur = nums2[p2++];
-    } else if (p2 === n) {
+    } else if (p2 === n) {//当数组2被遍历完，下一次遍历数组1，并去除当前值
       cur = nums1[p1++];
-    } else if (nums1[p1] < nums2[p2]) {
+    } else if (nums1[p1] < nums2[p2]) {//当数组1的指向小于数组2的指向，则下一次遍历数组1并取出当前值
       cur = nums1[p1++];
-    } else {
+    } else {//当数组1的指向大于数组2的指向，则下一次遍历数组2并取出当前值
       cur = nums2[p2++];
     }
+    //存放当前值
     sorted[p1 + p2 - 1] = cur;
   }
   for (let i = 0; i != m + n; ++i) {
+    //将sorted深拷贝给nums1
     nums1[i] = sorted[i];
   }
 };

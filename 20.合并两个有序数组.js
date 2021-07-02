1f17ef39fdd2c16d403948f5683e2abff6ac8f9f
rayhomie <1572801584@qq.com>
2021-07-02 10:17:20 +0800

    20

---
+++ 20.合并两个有序数组.js
@@ -0,0 +1,44 @@
+/*
+给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 的空间大小等于 m + n，这样它就有足够的空间保存来自 nums2 的元素。
+
+示例 1：
+输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+输出：[1,2,2,3,5,6]
+
+示例 2：
+输入：nums1 = [1], m = 1, nums2 = [], n = 0
+输出：[1]
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/merge-sorted-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+//方法一：暴力求解
+var merge = function (nums1, m, nums2, n) {
+  nums1.splice(m, nums1.length - m, ...nums2);
+  nums1.sort((a, b) => a - b);
+};
+
+
+//方法二：双指针
+var merge = function (nums1, m, nums2, n) {
+  let p1 = 0, p2 = 0;
+  const sorted = new Array(m + n).fill(0);
+  var cur;
+  while (p1 < m || p2 < n) {
+    if (p1 === m) {
+      cur = nums2[p2++];
+    } else if (p2 === n) {
+      cur = nums1[p1++];
+    } else if (nums1[p1] < nums2[p2]) {
+      cur = nums1[p1++];
+    } else {
+      cur = nums2[p2++];
+    }
+    sorted[p1 + p2 - 1] = cur;
+  }
+  for (let i = 0; i != m + n; ++i) {
+    nums1[i] = sorted[i];
+  }
+};

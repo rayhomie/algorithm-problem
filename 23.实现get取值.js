30f268a1bff2ec2a56904fe0d1efa652c58099e3
rayhomie <1572801584@qq.com>
2021-07-03 12:37:14 +0800

    get取值

--- 23.实现get取值.js
+++ 23.实现get取值.js
@@ -12,18 +12,14 @@ get(obj, "selector.to.val", "target[0]", "target[1][1]", "target[2].a");
 ["val to select", 1, 2, "test"];
 */
 
-const obj = {
-  selector: { to: { val: "val to select" } },
-  target: [1, [2, 3, 4], { a: "test" }],
-};
-
 function get(obj, ...args) {
   function handle(str) {
     return str //将字符串进行处理
-      .split(".")//处理.
-      .map((i) => i.split("[").map((i) => i.split("]").join("")))//处理[]
+      .split(".") //处理.
+      .map((i) => i.split("[").map((i) => i.split("]").join(""))) //处理[]
       .flat() //需要数组扁平化
-      .reduce((pre, key) => {//这里有个小技巧，使用reduce实现层次对象读取值
+      .reduce((pre, key) => {
+        //这里有个小技巧，使用reduce实现层次对象读取值
         //开始访问对象取值
         return pre[key];
       }, obj);
@@ -31,6 +27,10 @@ function get(obj, ...args) {
   return args.map(handle);
 }
 
+const obj = {
+  selector: { to: { val: "val to select" } },
+  target: [1, [2, 3, 4], { a: "test" }],
+};
 console.log(
   get(obj, "selector.to.val", "target[0]", "target[1][1]", "target[2].a")
 );

f6927caf52ad4e9b97a8923d8e86a903dfde8ddf
rayhomie <1572801584@qq.com>
2021-07-04 14:31:32 +0800

    剪枝操作

--- 回溯算法/1、组合.js
+++ 回溯算法/1、组合.js
@@ -16,6 +16,7 @@
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
 /**
+ * 回溯算法
  * https://www.bilibili.com/video/BV1ti4y1L7cv
  * @param {number} n
  * @param {number} k
@@ -31,10 +32,43 @@ var combine = function (n, k) {
       result.push([...path]); //需要深拷贝，否则就相当于push的是指针
       return;
     }
-    for (let i = startIndex; i < n; i++) {//遍历当前集合
-      path.push(i + 1);//将遍历的值加入path
-      com(n, k, i + 1);//递归加入下一次遍历的path
-      path.pop();//回溯逻辑：出栈（进行撤销）
+    for (let i = startIndex; i < n; i++) {
+      //遍历当前集合
+      path.push(i + 1); //将遍历的值加入path
+      com(n, k, i + 1); //递归加入下一次遍历的path
+      path.pop(); //回溯逻辑：出栈（进行撤销）
+    }
+  }
+  com(n, k, 0);
+  return result;
+};
+
+/**
+ * 回溯算法 + 剪枝
+ * https://www.bilibili.com/video/BV1ti4y1L7cv
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function (n, k) {
+  let result = []; //结果二维数组
+  let path = []; //一位数组，存储当前结果
+  function com(n, k, startIndex) {
+    //startIndex是指当前的开始位置，最初从0开始
+    if (path.length === k) {
+      //终止条件，当前结果的长度为k时，将其加入二维数组中
+      result.push([...path]); //需要深拷贝，否则就相当于push的是指针
+      return;
+    }
+    for (
+      let i = startIndex;
+      i + (k - path.length) < n + 1; //剪枝操作（k-path.length是还需要选取的个数）
+      i++
+    ) {
+      //遍历当前集合
+      path.push(i + 1); //将遍历的值加入path
+      com(n, k, i + 1); //递归加入下一次遍历的path
+      path.pop(); //回溯逻辑：出栈（进行撤销）
     }
   }
   com(n, k, 0);

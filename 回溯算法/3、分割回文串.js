1609e7aa818fcc8d6d463eb2ab13051384fa355d
rayhomie <1572801584@qq.com>
2021-07-04 15:37:08 +0800

    编程技巧

---
+++ 回溯算法/3、分割回文串.js
@@ -0,0 +1,21 @@
+/*
+给你一个字符串 s，请你将 s 分割成一些子串，使每个子串都是 回文串 。返回 s 所有可能的分割方案。
+回文串 是正着读和反着读都一样的字符串。
+示例 1：
+输入：s = "aab"
+输出：[["a","a","b"],["aa","b"]]
+示例 2：
+输入：s = "a"
+输出：[["a"]]
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/palindrome-partitioning
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+/**
+ * 回溯
+ * https://www.bilibili.com/video/BV1c54y1e7k6
+ * @param {string} s
+ * @return {string[][]}
+ */
+var partition = function (s) {};

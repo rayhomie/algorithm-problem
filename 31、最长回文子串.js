80c29fbc042adc9b6055044c7c971cc899831a47
rayhomie <1572801584@qq.com>
2021-08-18 21:48:49 +0800

    中心扩散法

--- 31、最长回文子串.js
+++ 31、最长回文子串.js
@@ -78,16 +78,63 @@ const longestPalindrome1 = (s) => {
  */
 
 const longestPalindrome2 = (s) => {
+  if (s.length < 2) {
+    return s;
+  }
   let max = "";
   for (var i = 0; i < s.length; i++) {
-  
-    while (i-1,i+1) {}
+    // 回文子串长度是奇数
+    helper(i, i);
+    // 回文子串长度是偶数
+    helper(i, i + 1);
+  }
+  function helper(m, n) {
+    while (m >= 0 && n < s.length && s[m] === s[n]) {
+      m--;
+      n++;
+    }
+    // 注意此处m,n的值循环完后  是恰好不满足循环条件的时刻
+    // 此时m到n的距离为n-m+1，但是mn两个边界不能取 所以应该取m+1到n-1的区间  长度是n-m-1
+    // slice也要取[m+1,n-1]这个区间
+    max = n - m - 1 > max.length ? s.substring(m + 1, n) : max;
   }
   return max;
 };
 
+/**
+ * 中心扩散法(终极优化)
+ * @param {string} s
+ * @return {string}
+ */
+
+const longestPalindrome3 = (s) => {
+  if (s.length < 2) {
+    return s;
+  }
+  let l = 0;
+  let r = 0;
+  for (var i = 0; i < s.length; i++) {
+    // 回文子串长度是奇数
+    helper(i, i);
+    // 回文子串长度是偶数
+    helper(i, i + 1);
+  }
+  function helper(m, n) {
+    while (m >= 0 && n < s.length && s[m] === s[n]) {
+      m--;
+      n++;
+    }
+    // 注意此处m,n的值循环完后  是恰好不满足循环条件的时刻 如果此轮询得到回文串长度大于之前记录， 记录此轮循边界
+    if (n - m - 1 > r - l - 1) {
+      r = n;
+      l = m;
+    }
+  }
+  return s.substring(l + 1, r);
+};
+
 console.log(
-  longestPalindrome1(
+  longestPalindrome2(
     "kyyrjtdplseovzwjkykrjwhxquwxsfsorjiumvxjhjmgeueafubtonhlerrgsgohfosqssmizcuqryqomsipovhhodpfyudtusjhonlqabhxfahfcjqxyckycstcqwxvicwkjeuboerkmjshfgiglceycmycadpnvoeaurqatesivajoqdilynbcihnidbizwkuaoegmytopzdmvvoewvhebqzskseeubnretjgnmyjwwgcooytfojeuzcuyhsznbcaiqpwcyusyyywqmmvqzvvceylnuwcbxybhqpvjumzomnabrjgcfaabqmiotlfojnyuolostmtacbwmwlqdfkbfikusuqtupdwdrjwqmuudbcvtpieiwteqbeyfyqejglmxofdjksqmzeugwvuniaxdrunyunnqpbnfbgqemvamaxuhjbyzqmhalrprhnindrkbopwbwsjeqrmyqipnqvjqzpjalqyfvaavyhytetllzupxjwozdfpmjhjlrnitnjgapzrakcqahaqetwllaaiadalmxgvpawqpgecojxfvcgxsbrldktufdrogkogbltcezflyctklpqrjymqzyzmtlssnavzcquytcskcnjzzrytsvawkavzboncxlhqfiofuohehaygxidxsofhmhzygklliovnwqbwwiiyarxtoihvjkdrzqsnmhdtdlpckuayhtfyirnhkrhbrwkdymjrjklonyggqnxhfvtkqxoicakzsxmgczpwhpkzcntkcwhkdkxvfnjbvjjoumczjyvdgkfukfuldolqnauvoyhoheoqvpwoisniv"
   )
 );
